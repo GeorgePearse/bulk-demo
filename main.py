88a361015f361981a87bf07c7d7fcdb056430cdc
George Pearse <47161914+GeorgePearse@users.noreply.github.com>
2022-09-18 15:32:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from umap import UMAP
-
 # pip install -U sentence-transformers
 from sentence_transformers import SentenceTransformer
 
 # Load the universal sentence encoder
-model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+# Stay well clear of the direct Hugging Face API which is grim
+sample_size = 30000
+model = SentenceTransformer('all-mpnet-base-v2')
+
+embeddings = model.encode(sentences[:sample_size])
 
 # Load original dataset
 df = pd.read_csv("original.csv")
@@ -15,10 +18,11 @@ sentences = df["text"]
 X =  model.encode(sentences)
 
 # Reduce the dimensions with UMAP
-umap = UMAP()
-X_tfm = umap.fit_transform(X)
+umap = UMAP(n_components=2, verbose=True)
+X_tfm = umap.fit_transform(embeddings)
 
 # Apply coordinates
+df = df.iloc[:sample_size]
 df['x'] = X_tfm[:, 0]
 df['y'] = X_tfm[:, 1]
 df.to_csv("ready.csv")
